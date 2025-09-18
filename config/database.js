2a3d521226a952d200c7c6e97decf5d39419b4f0
Muhammad Muneeb <112570402+muneeb6624@users.noreply.github.com>
2025-09-19 04:08:24 +0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -18,8 +18,8 @@ const connectDB = async () => {
 
     const conn = await mongoose.connect(process.env.MONGODB_URI, {
       maxPoolSize: 10, // Maintain up to 10 socket connections
-      serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-      socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+      serverSelectionTimeoutMS: 20000, // Keep trying to send operations for 5 seconds
+      socketTimeoutMS: 60000, // Close sockets after 45 seconds of inactivity
     });
 
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
